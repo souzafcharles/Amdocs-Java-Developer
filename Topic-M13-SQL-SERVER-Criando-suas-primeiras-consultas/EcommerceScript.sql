a92c7264ef5d1a60d83ac2676153f60daf09781d
Charles Fernandes de Souza <souzach@outlook.com>
2022-01-15 00:36:31 -0300

    SQL SERVER - Criando suas primeiras consultas

--- Topic-M13-SQL-SERVER-Criando-suas-primeiras-consultas/EcommerceScript.sql
+++ Topic-M13-SQL-SERVER-Criando-suas-primeiras-consultas/EcommerceScript.sql
@@ -139,7 +139,7 @@ select	*
 	where Preco between 1 and 2
 
 select	sum(Preco * Quantidade) 
-	from  PedidoItem t1
+	from  	PedidoItem t1
 	inner join PedidoItemLog t2
 	on	  t1.CodigoPedido = t2.CodigoPedido
 	and	  t1.CodigoProduto = t2.CodigoProduto
@@ -147,53 +147,53 @@ select	sum(Preco * Quantidade)
 	on	  t3.Codigo = t2.CodigoStatusPedidoItem
 
 select	avg(Preco) 
- from  PedidoItem t1
- inner join PedidoItemLog t2
- on    t1.CodigoPedido = t2.CodigoPedido
- and   t1.CodigoProduto = t2.CodigoProduto
- inner join StatusPedidoItem t3
- on    t3.Codigo = t2.CodigoStatusPedidoItem
+ 	from  	PedidoItem t1
+ 	inner 	join PedidoItemLog t2
+ 	on    	t1.CodigoPedido = t2.CodigoPedido
+ 	and   	t1.CodigoProduto = t2.CodigoProduto
+ 	inner 	join StatusPedidoItem t3
+ 	on    	t3.Codigo = t2.CodigoStatusPedidoItem
 
 select	count(*) 
- from	PedidoItem t1
- inner	join PedidoItemLog t2
- on		t1.CodigoPedido = t2.CodigoPedido
- and	t1.CodigoProduto = t2.CodigoProduto
- inner	join StatusPedidoItem t3
- on		t3.Codigo = t2.CodigoStatusPedidoItem
+ 	from	PedidoItem t1
+ 	inner	join PedidoItemLog t2
+ 	on	t1.CodigoPedido = t2.CodigoPedido
+ 	and	t1.CodigoProduto = t2.CodigoProduto
+ 	inner	join StatusPedidoItem t3
+ 	on	t3.Codigo = t2.CodigoStatusPedidoItem
 
 select	* 
- from	PedidoItem t1
- inner	join PedidoItemLog t2
- on		t1.CodigoPedido = t2.CodigoPedido
- and	t1.CodigoProduto = t2.CodigoProduto
- inner	join StatusPedidoItem t3
- on		t3.Codigo = t2.CodigoStatusPedidoItem
- inner	join Produtos t4
- on		t4.Codigo = t2.CodigoProduto
+ 	from	PedidoItem t1
+ 	inner	join PedidoItemLog t2
+ 	on	t1.CodigoPedido = t2.CodigoPedido
+ 	and	t1.CodigoProduto = t2.CodigoProduto
+	inner	join StatusPedidoItem t3
+	on	t3.Codigo = t2.CodigoStatusPedidoItem
+ 	inner	join Produtos t4
+ 	on	t4.Codigo = t2.CodigoProduto
 
 select	t4.Codigo,
-		t4.Descricao,
-		t1.Preco * t1.Quantidade
- from	PedidoItem t1
- inner	join PedidoItemLog t2
- on		t1.CodigoPedido = t2.CodigoPedido
- and		t1.CodigoProduto = t2.CodigoProduto
- inner	join StatusPedidoItem t3
- on		t3.Codigo = t2.CodigoStatusPedidoItem
- inner	join Produtos t4
- on		t4.Codigo = t2.CodigoProduto
+	t4.Descricao,
+	t1.Preco * t1.Quantidade
+ 	from	PedidoItem t1
+ 	inner	join PedidoItemLog t2
+ 	on	t1.CodigoPedido = t2.CodigoPedido
+ 	and	t1.CodigoProduto = t2.CodigoProduto
+ 	inner	join StatusPedidoItem t3
+ 	on	t3.Codigo = t2.CodigoStatusPedidoItem
+ 	inner	join Produtos t4
+ 	on	t4.Codigo = t2.CodigoProduto
 
 select	t4.Codigo,
-		t4.Descricao,
-		sum(t1.Preco * t1.Quantidade) 
- from	PedidoItem t1
- inner	join PedidoItemLog t2
- on		t1.CodigoPedido = t2.CodigoPedido
- and	t1.CodigoProduto = t2.CodigoProduto
- inner	join StatusPedidoItem t3
- on		t3.Codigo = t2.CodigoStatusPedidoItem
- inner	join Produtos t4
- on		t4.Codigo = t2.CodigoProduto
- group	by t4.Codigo,
-		t4.Descricao
+	t4.Descricao,
+	sum(t1.Preco * t1.Quantidade) 
+	from	PedidoItem t1
+	inner	join PedidoItemLog t2
+	on	t1.CodigoPedido = t2.CodigoPedido
+	and	t1.CodigoProduto = t2.CodigoProduto
+	inner	join StatusPedidoItem t3
+	on	t3.Codigo = t2.CodigoStatusPedidoItem
+	inner	join Produtos t4
+	on	t4.Codigo = t2.CodigoProduto
+	group	by t4.Codigo, t4.Descricao
+	order	by 2 desc
