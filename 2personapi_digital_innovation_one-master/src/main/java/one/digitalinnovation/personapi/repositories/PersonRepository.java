f8a6f95b6755af0aa6fa6d94f32f7002fbfabd9e
Charles Fernandes de Souza <souzach@outlook.com>
2022-01-16 22:16:39 -0300

    Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot

--- 2personapi_digital_innovation_one-master/src/main/java/one/digitalinnovation/personapi/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package one.digitalinnovation.personapi.repositories;
-
-import one.digitalinnovation.personapi.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
